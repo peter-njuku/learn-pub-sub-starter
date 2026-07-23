d95806e758e96b5db94bbfce5c49b82be5feb284
peter-njuku <njuku2005@gmail.com>
2026-07-23 15:49:53 +0300

    STOMP protocol enabled

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+	connectionStr := "amqp://guest:guest@localhost:5672/"
+	amqp, err := amqp.Dial(connectionStr)
+	if err != nil {
+		log.Print(err)
+	}
+	defer amqp.Close()
+	fmt.Println("Connection successfull")
+	_, err = amqp.Channel()
+	if err != nil {
+		log.Print(err)
+	}
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, os.Interrupt)
+	<-sigCh
+	fmt.Println("Signal received. Shutting down...")
 }
